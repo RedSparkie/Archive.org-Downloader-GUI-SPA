4cd2f8cf53e79ad1223e98acc727128c81f67e88
alo-beep <alowaynal@outlook.com>
2022-01-22 14:34:36 -0800

    Implemented basic GUI look, no functionality

--- GUI.py
+++ GUI.py
@@ -1,43 +1,62 @@
 #!/usr/bin/python3
 
-import tkinter as tk  # note that module name has changed from Tkinter
-                # in Python 2 to tkinter in Python 3\
-root = tk.Tk()
-root.title("Archive.org-Downloader")
-# Code to add widgets will go here...
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import ttk
+from tkinter import Menu
 
-content = tk.Frame(root)
-urlFrame = tk.Frame(content, borderwidth=5, relief="ridge"
-    , width=600, height=400)
-displayFrame = tk.Frame(content, borderwidth=5, relief="ridge"
-    , width=600, height=100)
-emailLabel = tk.Label(content, text="Email")
-emailEntry = tk.Entry(content)
-passwordLabel = tk.Label(content, text="Password")
-passwordEntry = tk.Entry(content, show="*")
+# initial window declaration
+window = tk.Tk()
+window.title("Archive.org-DLG")
+window.geometry("600x400")
 
-# placing contents of GUI on grid
-content.grid(column=0, row=0)
-    # login
-emailLabel.grid(column=0, row=0, columnspan=1, sticky=(tk.E))
-emailEntry.grid(column=1, row=0, columnspan=1
-    , sticky=(tk.N, tk.E, tk.W), padx = 5)
-passwordLabel.grid(column=2, row=0, columnspan=1
-    , sticky=(tk.N, tk.E, tk.W))
-passwordEntry.grid(column=3, row=0, columnspan=1
-    , sticky=(tk.N, tk.E, tk.W), padx = 5)
-    # url
-urlFrame.grid(column=0, row=1, columnspan=3, rowspan=2)
-    # display
-displayFrame.grid(column=0, row=3, columnspan=3, rowspan=2)
+# menu declaration
+menu = tk.Menu(window)
 
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-content.columnconfigure(0, weight=3)
-content.columnconfigure(1, weight=3)
-content.columnconfigure(2, weight=3)
-content.columnconfigure(3, weight=1)
-content.columnconfigure(4, weight=1)
-content.rowconfigure(1, weight=1)
+fileMenuElements = tk.Menu(menu)
+fileMenuElements.add_command(label="Load File")
+menu.add_cascade(label="File", menu=fileMenuElements)
 
-root.mainloop()
+optionsMenuElements = tk.Menu(menu)
+optionsMenuElements.add_command(label="Download Resolution")
+optionsMenuElements.add_command(label="Toggle JPG/PDF")
+optionsMenuElements.add_command(label="Threads")
+menu.add_cascade(label="Options", menu=optionsMenuElements)
+
+menu.add_command(label="Help")
+
+window.config(menu=menu)
+
+# frame declarations
+loginFrame = tk.LabelFrame(window, text="Login", width=50, height=50)
+loginFrame.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5, ipady=5)
+downloadFrame = tk.Frame(window, width=50, height=50)
+downloadFrame.pack(side=tk.BOTTOM, padx=5, pady=5)
+urlFrame = tk.LabelFrame(window, text="URL(s)", padx=5, pady=5, width=50, height=50)
+urlFrame.pack(side=tk.BOTTOM, fill=tk.BOTH, padx=5, pady=5)
+
+# logins frame widgets
+emailLabel = tk.Label(loginFrame, text="Email: ")
+emailLabel.pack(side=tk.LEFT)
+emailEntry = tk.Entry(loginFrame, width=30)
+emailEntry.pack(side=tk.LEFT, fill=tk.X)
+passwordLabel = tk.Label(loginFrame, text="Password:")
+passwordLabel.pack(side=tk.LEFT)
+passwordEntry = tk.Entry(loginFrame, width=30, show="*")
+passwordEntry.pack(side=tk.LEFT, fill=tk.X)
+
+# urls frame widgets
+pasteButton = tk.Button(urlFrame, text="Paste")
+pasteButton.pack(side=tk.LEFT, fill=tk.Y)
+urlText = scrolledtext.ScrolledText(urlFrame, width=40, height=30)
+urlText.pack(side=tk.TOP, fill=tk.BOTH)
+
+# download frame widgets
+startButton = tk.Button(downloadFrame, text="Start Download")
+startButton.pack(side=tk.LEFT)
+openLocationButton = tk.Button(downloadFrame, text="Open Download Location")
+openLocationButton.pack(side=tk.LEFT)
+progressBar = ttk.Progressbar(downloadFrame, orient=tk.HORIZONTAL, length=400)
+progressBar.pack(side=tk.LEFT, fill=tk.X)
+
+window.mainloop()
